12cd491fda4de0b19b9027557cb3a1cb97f3ef05
karanomorishion <karanomori@users.noreply.github.com>
2021-01-23 12:47:44 +0800

    new problem: https://leetcode-cn.com/problems/merge-sorted-array/

---
+++ Week_01/merge_sorted_array.py
@@ -0,0 +1,12 @@
+# 88. Merge Sorted Array (Easy)
+# https://leetcode-cn.com/problems/merge-sorted-array/
+
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2)
